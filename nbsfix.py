5a1b1fc933b7780de8338cad9a11e420e871187d
Eilles <EillesWan@outlook.com>
2026-07-05 04:35:52 +0800

    1.0.1 double run check

--- nbsfix.py
+++ nbsfix.py
@@ -15,8 +15,12 @@ limitations under the License.
 
 """
 
-print("本脚本依照以下开源协议发布，请阅读后按下回车键表示同意、同时按下 `Ctrl` 和 `C` 键表示不同意并退出本脚本。")
-print("The Script is Licensed under the Following Open Source License. Press `Enter` to Agree, `Ctrl+C` to Cancel and Quit.")
+print(
+    "本脚本依照以下开源协议发布，请阅读后按下回车键表示同意、同时按下 `Ctrl` 和 `C` 键表示不同意并退出本脚本。"
+)
+print(
+    "The Script is Licensed under the Following Open Source License. Press `Enter` to Agree, `Ctrl+C` to Cancel and Quit."
+)
 input(COPYRIGNT)
 print("\033[2J\033[H", end="", flush=True)
 
@@ -28,7 +32,6 @@ def main():
     ori_roaming_path = Path(
         environ.get("APPDATA", "C:\\Users\\{}\\AppData\\Roaming".format(getlogin()))
     ).absolute()
-
 
     print("已获取漫游目录于 | Having Get %APPDATA% Path at:", ori_roaming_path)
 
@@ -46,22 +49,32 @@ def main():
         print("!!! 你无需进行此修复 | Your %APPDATA% Path is OK !!!")
         print('毕竟你的用户名很西方 | You Dont Need to "Fix" Your %APPDATA% Path')
         return 0
-    if not (opth := (ori_roaming_path / "Minecraft_Note_Block_Studio").resolve()).exists():
+    if not (
+        opth := (ori_roaming_path / "Minecraft_Note_Block_Studio").resolve()
+    ).exists():
         print("!!! 请先运行一遍 NBS | You Might Never Run NBS Before !!!")
         print("如果你没有运行过 NBS，那是怎么知道需要修复路径问题的？")
         print(
-            "Please Run NBS at Least Once Before You Want to Fix Your %APPDATA% Path Problem."
+            "Please Run NBS at Least Once "
+            "Before You Want to Fix Your %APPDATA% Path Problem."
         )
         return 0
-
+    if (
+        epth := ansi_roeaming_path / "Minecraft_Note_Block_Studio"
+    ).exists() and epth.is_symlink():
+        print("!!! 请不要重复运行本修复脚本 | Please Don't Run This Script Again !!!")
+        print("已经修过了，就别再修了，唧唧复唧唧，木兰都能唧。")
+        print(
+            "If You Have Already Fixed This, Don't Run the Script Again; "
+            "If You Havn't, NBS will Not Work Inproperly."
+        )
+        return 0
 
     ansi_roeaming_path.mkdir(parents=True, exist_ok=True)
 
     print("已创建所需的上级目录 | Created Error ANSI Parent Path")
 
-    (epth := ansi_roeaming_path / "Minecraft_Note_Block_Studio").symlink_to(
-        opth, target_is_directory=True
-    )
+    epth.symlink_to(opth, target_is_directory=True)
 
     if opth.exists() and epth.exists():
         print("已创建符号链接 | Created Symlink of `{}` to `{}`".format(opth, epth))
@@ -75,4 +88,3 @@ def main():
 if __name__ == "__main__":
     main()
     input("完成，按回车键退出 | All Done, Press `Enter` to Exit")
-
